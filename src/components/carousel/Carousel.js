ccc16a06408322fde97077b3fe06a44e9899aabe
Cedrick Monesit <cedrickmonesit@gmail.com>
2022-09-24 15:56:14 -0400

    Carousel component
    Created a carousel component using slick slider creating a dynamic carousel that can take data from API calls

--- src/components/carousel/Carousel.js
+++ src/components/carousel/Carousel.js
@@ -1,17 +1,51 @@
 import Slider from "react-slick";
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import Card from "../card/Card";
-import { render } from "@testing-library/react";
+import "./carousel.scss";
 
 const Carousel = ({ items }) => {
   const renderSlider = (products) => {
     if (products) {
       const settings = {
         dots: true,
-        infinite: true,
+        infinite: false,
         speed: 500,
-        slidesToShow: 3,
-        slidesToScroll: 3,
+        slidesToShow: 6,
+        slidesToScroll: 6,
+        initialSlide: 0,
+        swipeToSlide: true,
+        responsive: [
+          {
+            breakpoint: 1150,
+            settings: {
+              slidesToShow: 5,
+              slidesToScroll: 5,
+              infinite: true,
+              dots: true,
+            },
+          },
+          {
+            breakpoint: 980,
+            settings: {
+              slidesToShow: 4,
+              slidesToScroll: 4,
+            },
+          },
+          {
+            breakpoint: 850,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 3,
+            },
+          },
+          {
+            breakpoint: 400,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+            },
+          },
+        ],
       };
 
       console.log(products);
